df3c32cef657ee6f4d1065daab60c914ffc38a07
Graham Searancke <g.searancke@gmail.com>
2026-09-05 13:30:39 +1200

    add not id checks of server routes

--- server/routes/boardgames.ts
+++ server/routes/boardgames.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-// import { boardgame } from '../../models/boardgames'
 import * as db from '../db/db'
 
 const router = express.Router()
@@ -21,8 +20,12 @@ router.get('/', async (req, res) => {
 
 // GET /api/v1/boardgames/id
 router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  if (!id) {
+    console.error(id + ' is an invalid id')
+    return res.status(400).send('Bad request')
+  }
   try {
-    const id = Number(req.params.id)
     const game = await db.getBoardgameById(id)
     res.json(game)
   } catch (error: unknown) {
@@ -56,21 +59,9 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
-// POST http://localhost:3000/api/v1/boardgames
-// {
-//     "name": "5-Minute Dungeon",
-//     "playerCount": "2-5",
-//     "playTime": "5-30",
-//     "category": "",
-//     "personalRating": "",
-//     "bggRating": "6.89",
-//     "status": "owned (for trade)"
-//     "bgg_objectid": "",
-// }
 router.post('/', async (req, res) => {
   try {
     const boardgame = await db.addBoardgame(req.body)
-
     res.status(201).json(boardgame)
   } catch (error: unknown) {
     if (error instanceof Error) {
